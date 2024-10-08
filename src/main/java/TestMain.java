ed2795109c69968e8759639aeadd6806a3492971
AnnLee <1772527675@qq.com>
2024-10-08 11:09:47 +0800

    第一次正式提交

---
+++ src/main/java/TestMain.java
@@ -0,0 +1,18 @@
+/**
+ * <b>
+ * <p>TODO</p>
+ * </b>:
+ * <p>@ClassName:  TestMain</p>
+ * <p>@PackageName:PACKAGE_NAME</p>
+ *
+ * <p>@author:     AnnLee</p>
+ * <p>@date:       2024/10/8</p>
+ * <p>@link:       </p>
+ */
+public class TestMain {
+    /* ---------------- Constants -------------- */
+
+    public static void main(String[] args) {
+        System.out.println("这是第一次提交代码");
+    }
+}
